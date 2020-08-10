1311bec1d6868a40e188d676f17d992b51ec3a95
chandanishukla <69348852+chandanishukla@users.noreply.github.com>
2020-08-10 18:56:07 +0530

    Update Assignment6.py

--- Assignment6.py
+++ Assignment6.py
@@ -1,17 +1,17 @@
 
 
-## remove duplicate value from the list
+## program to  remove duplicate value from the list
 l=[3,2,5,4,7,6,3,5]
 l = list(dict.fromkeys(l))
 print(l)
 
-#find biggest value of a given value using lambda function
+# program to find biggest value of a given value using lambda function
 x=lambda a,b:a if a>b else b
 print(x(4,3))
 
 
 
-# number of different vowels in the given string
+# program to find  number of different vowels in the given string
 vowels =['a','e','i','o','u']
 a= input("Enter any Elements : ")
 value = set(a)
