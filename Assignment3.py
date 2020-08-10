7ecec11cdeed59f6c8da2bbd04491446cfe8bfad
chandanishukla <69348852+chandanishukla@users.noreply.github.com>
2020-08-10 18:53:21 +0530

    Update Assignment3.py

--- Assignment3.py
+++ Assignment3.py
@@ -1,14 +1,14 @@
-# reverse the string in descending order
+# program to reverse the string in descending order
 str='python django'
 st="   ".join(reversed(sorted(str)))
 print(st)
 
 
-#reverse the string
+# program to reverse the string
 st="hello python"
 print(' '.join(reversed(st)))
 
 
-# reverse string in ascending order
+#  program to reverse string in ascending order
 str='hello python'
 st="  ".join(sorted(str))
